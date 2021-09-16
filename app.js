1effc985f54ca58c274319c4378329fac7681c3d
Nathan Flessner <nathanflessner1@gmail.com>
2021-09-15 18:02:48 -0600

    added connection to sql in app.js

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ require('dotenv').config();
 const mySql = require('mysql2')
 const inquirer = require('inquirer'); 
 const consoleTable = require('console.table');
+
+const connection = mySql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
